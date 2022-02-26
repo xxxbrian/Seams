ca735d014fadc05c11e15e8ab4cc9c3c2f90154d
Bojin Li <bojin.li@foxmail.com>
2022-02-27 01:48:09 +0800

    Add normal test

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -40,3 +40,22 @@ def test_channels_create_name_too_long(clear, create_user):
     with pytest.raises(InputError):
         for user in create_user:
             channels_create_v1(user['auth_user_id'], name, True)
+
+
+def test_channels_create_all_normal(clear, create_user):
+    channel_list = list()
+    for user in create_user:
+        channel_list.append(
+            channels_create_v1(user['auth_user_id'], 'Tesla', True))
+        # different channel name
+        channel_list.append(
+            channels_create_v1(user['auth_user_id'], 'SpaceX', True))
+        # different channel type
+        channel_list.append(
+            channels_create_v1(user['auth_user_id'], 'SpaceX', False))
+        # same channel name
+        channel_list.append(
+            channels_create_v1(user['auth_user_id'], 'SpaceX', False))
+    for channel in channel_list:
+        assert type(channel) == dict
+        assert type(channel['channel_id']) == int
