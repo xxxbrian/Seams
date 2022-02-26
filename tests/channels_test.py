79782aae2a23a5b4a29776dee863ef1c6039fea4
Bojin Li <bojin.li@foxmail.com>
2022-02-27 01:43:08 +0800

    Update import

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -1,4 +1,13 @@
 import pytest
 
 from src.channels import channels_create_v1
+from src.auth import auth_register_v1
 from src.error import InputError
+from src.other import clear_v1
+
+import random, string
+
+
+@pytest.fixture
+def clear():
+    clear_v1
