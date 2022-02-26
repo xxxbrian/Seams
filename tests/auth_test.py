a90972772d95e5a0791e398b700d47a1d23d6fbe
Steve <z5374603@ad.unsw.edu.au>
2022-02-26 15:26:31 +0800

    auth login test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,12 +1,7 @@
-from unicodedata import name
 import pytest
 from src.auth import auth_login_v1, auth_register_v1
 from src.other import clear_v1
 from src.error import InputError
-
-# @pytest.fixture()
-# def clean():
-    # clear_v1()
     
 @pytest.fixture(name = "user_list")
 def create_users():
@@ -16,6 +11,7 @@ def create_users():
     Return a list of user_id (type: dict)
 
     """
+    clear_v1()
     user_list = list()
     user_list.append(auth_register_v1("z5374603@ad.unsw.edu.au", "Ymc123", "Steve", "Yang"))
     user_list.append(auth_register_v1("z5201314@ad.unsw.edu.au", "Bojin123", "Bojin", "Li"))
@@ -23,36 +19,41 @@ def create_users():
     user_list.append(auth_register_v1("13579@gmail.com", "Lebron123", "Lebron", "James"))
     return user_list
 
-# @pytest.mark.usefixtures("clear")
-class TestLogin:
-    @pytest.mark.usefixtures("user_list")
-    def test_login_user_id(self, user_list):
-        clear_v1()
-        assert auth_login_v1("z5374603@ad.unsw.edu.au", "Ymc123") == user_list[0]
-        assert auth_login_v1("z5201314@ad.unsw.edu.au", "Bojin123") == user_list[1]
-        assert auth_login_v1("12345678@qq.com", "Cicy123") == user_list[2]
-        assert auth_login_v1("13579@gmail.com", "Lebron123") == user_list[3]
+def test_login_user_id(user_list):
+    assert auth_login_v1("z5374603@ad.unsw.edu.au", "Ymc123") == user_list[0]
+    assert auth_login_v1("z5201314@ad.unsw.edu.au", "Bojin123") == user_list[1]
+    assert auth_login_v1("12345678@qq.com", "Cicy123") == user_list[2]
+    assert auth_login_v1("13579@gmail.com", "Lebron123") == user_list[3]
     
-    @pytest.mark.usefixtures("user_list")
-    def test_login_inexistent_email(self):
-        with pytest.raises(InputError):
-            assert auth_login_v1("", "asdfg")   # test empty email
-        with pytest.raises(InputError):
-            assert auth_login_v1("email", "123456") # test string
-        with pytest.raises(InputError):
-            assert auth_login_v1("email.com", "1234")   # test without '@'
-        with pytest.raises(InputError):
-            assert auth_login_v1("email@qq", "1234")    # test without '.com'
-        with pytest.raises(InputError):
-            assert auth_login_v1("z5374604@ad.unsw.edu.au", "1234") # a valid email but inexistent
-           
-    @pytest.mark.usefixtures("user_list")        
-    def test_login_wrong_password(self):
-        with pytest.raises(InputError):
-            assert auth_login_v1("z5374603@ad.unsw.edu.au", "") # test empty password
-        with pytest.raises(InputError):
-            assert auth_login_v1("z5374603@ad.unsw.edu.au", "Ymc123")   # correct email but wrong password
-        with pytest. raises(InputError):
-            assert auth_login_v1("z5201314@ad.unsw.edu.au", "bojin123")
-        with pytest. raises(InputError):
-            assert auth_login_v1("13579@gmail.com", "Lebron")
+    
+def test_login_empty_email():
+    with pytest.raises(InputError):
+        assert auth_login_v1("", "asdfg")   # test empty email
+        
+def test_login_string_email():        
+    with pytest.raises(InputError):
+        assert auth_login_v1("email", "123456") # test string
+        
+def test_login_without_at_email():        
+    with pytest.raises(InputError):
+        assert auth_login_v1("email.com", "1234")   # test without '@'
+
+def test_login_without_com_email():
+    with pytest.raises(InputError):
+        assert auth_login_v1("email@qq", "1234")    # test without '.com'
+
+def test_login_inexistent_email():
+    with pytest.raises(InputError):
+        assert auth_login_v1("z5374604@ad.unsw.edu.au", "1234") # a valid email but inexistent
+                   
+def test_login_empty_password():
+    with pytest.raises(InputError):
+        assert auth_login_v1("z5374603@ad.unsw.edu.au", "") # test empty password
+        
+def test_login_correct_email_wrong_password():
+    with pytest.raises(InputError):
+        assert auth_login_v1("z5374603@ad.unsw.edu.au", "Ymc123")   # correct email but wrong password
+    with pytest. raises(InputError):
+        assert auth_login_v1("z5201314@ad.unsw.edu.au", "bojin123")
+    with pytest. raises(InputError):
+        assert auth_login_v1("13579@gmail.com", "Lebron")
