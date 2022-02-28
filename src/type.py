d5c8d19829a4584090ae8eaeaaba7814bd3c1480
Bojin Li <bojin.li@foxmail.com>
2022-02-28 23:51:52 +0800

    Add some methods to auth user

--- src/type.py
+++ src/type.py
@@ -1,6 +1,8 @@
 from src import data_store
 from src.data_store import data_store
 
+import re
+
 store = data_store.get()
 
 
@@ -31,13 +33,14 @@ class User():
         
     '''
 
-    def __init__(self, email, password, name_first, name_last) -> None:
-        self.u_id = User.get_last_id()
+    def __init__(self, email: str, password: str, name_first: str,
+                 name_last: str) -> None:
+        self.u_id = User.get_last_id() + 1
         self.email = email
         self.password = password
         self.name_first = name_first
         self.name_last = name_last
-        self.handle = self.generat_handle()
+        self.handle_str = self.generat_handle()
 
     @staticmethod
     def find_by_id(u_id):
@@ -51,19 +54,39 @@ class User():
         Return:
             User
         '''
-        return User
+        for user in store['users']:
+            if user.u_id == u_id:
+                return user
+        return None
+
+    @staticmethod
+    def find_by_email(email: str):
+        '''
+
+        Find user by user's email
+
+        Args: 
+            email
+
+        Return:
+            User
+        '''
+        for user in store['users']:
+            if user.email == email:
+                return user
+        return None
 
     @staticmethod
     def get_last_id() -> int:
         users = list(reversed(store['users']))
-        u_id = users[0].u_id if len(users) > 0 else 0
+        u_id = users[0].u_id if len(users) > 0 else -1
         return u_id
 
     def add_to_store(self):
         store['users'].append(self)
 
     @staticmethod
-    def generat_20fullname(name_first, name_last):
+    def generat_20fullname(name_first: str, name_last: str):
         fullname = (name_first + name_last).lower()
         fullname = ''.join(list(filter(str.isalnum, fullname)))[:20]
         return fullname
@@ -80,6 +103,45 @@ class User():
 
         lastend = str(int(lastend) + 1) if lastend != '' else lastend
         return fullname + lastend
+
+    @staticmethod
+    def check_email_invalid(email: str) -> bool:
+        """
+        Check whether the email address is valid
+        """
+        regx = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
+        return not re.fullmatch(regx, email)
+
+    @staticmethod
+    def check_email_been_used(email: str) -> bool:
+        """
+        Check whether the user's email is been used
+        """
+        return (User.find_by_email(email) != None)
+
+    @staticmethod
+    def check_password_invalid(password: str) -> bool:
+        """
+        Check whether the password is satisfy the length request
+        """
+        return len(password) < 6
+
+    @staticmethod
+    def check_name_invalid(name: str) -> bool:
+        """
+        Check whether the length of user is satisfy length request
+        """
+        return len(name) < 1 or len(name) > 50
+
+    @staticmethod
+    def match_email_password(email: str, password: str):
+        """
+        Check whether the input email and password are match
+        """
+        user = User.find_by_email(email)
+        if user:
+            return user.password == password
+        return False
 
 
 class Channel():
@@ -105,7 +167,7 @@ class Channel():
 
     '''
 
-    def __init__(self, u_id, name, is_public=True) -> None:
+    def __init__(self, u_id: int, name: str, is_public: bool = True) -> None:
         self.name = name
         self.owners = [User.find_by_id(u_id)]
         self.menbers = [self.owners]
