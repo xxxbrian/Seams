55436b0669d000183ede52ec17983238ee15c353
Cicy Zhou <cicy.zhou7@gmail.com>
2022-03-03 16:22:16 +1100

    completed test for clear_v1()

--- tests/other_test.py
+++ tests/other_test.py
@@ -1,7 +1,15 @@
-import pytest
 
+import pytest
 from src.other import clear_v1
+from src.data_store import data_store
 
 def test_clear_v1():
+    store = data_store.get()
+    users = store['users']
+    chann = store['channels']
+    users.append("users_test")
+    chann.append('chann_test')
+    clear_v1()
+    assert store['users'] == []
+    assert store['channels'] == []
     
-    pass
