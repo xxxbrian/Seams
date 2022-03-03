f2feb3bdd538809853d1bad9e47ba9019d2a6de2
Steve <z5374603@ad.unsw.edu.au>
2022-03-03 11:29:04 +0800

    finish 3/4 channel and fix some bugs in test

--- src/channel.py
+++ src/channel.py
@@ -1,24 +1,40 @@
+from src.error import InputError, AccessError
 from src.type import User, Channel
 
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
-    # Example for add someone in channel
-    channel = Channel.find_by_id(channel_id)
     user = User.find_by_id(u_id)
+    channel = Channel.find_by_id(channel_id)
+    if channel is None:
+        raise InputError
+    if user is None:
+        raise InputError
+    if channel.has_user(u_id) is True:
+        raise InputError
+    if Channel.find_by_id(channel_id) is not None and channel.has_user(auth_user_id) is False:
+        raise AccessError
+
     channel.join(user)
     return {}
 
 
 def channel_details_v1(auth_user_id, channel_id):
-    # Example for get channel details
     channel = Channel.find_by_id(channel_id)
+    if channel is None:
+        raise InputError
+    if channel.has_user(auth_user_id) == False:
+        raise AccessError
+    
     channel_info = channel.todict(
         {'name', 'is_public', 'owner_members', 'all_members'})
     return channel_info
 
 
 def channel_messages_v1(auth_user_id, channel_id, start):
-    # Wait for new methods provide in src.type
+    channel = Channel.find_by_id(channel_id)
+    if channel is None:
+        raise InputError
+    
     return {
         'messages': [
             {
@@ -34,5 +50,14 @@ def channel_messages_v1(auth_user_id, channel_id, start):
 
 
 def channel_join_v1(auth_user_id, channel_id):
-    # Example is same in channel_intive_v1
+    channel = Channel.find_by_id(channel_id)
+    if channel is None:
+        raise InputError
+    if Channel.has_user(channel, auth_user_id) is True:
+        raise InputError
+    if channel.is_public is False and channel.has_user(auth_user_id) is False:
+        raise AccessError
+    
+    user = User.find_by_id(auth_user_id)
+    channel.join(user)
     return {}
--- tests/channel_details_v1_test.py
+++ tests/channel_details_v1_test.py
@@ -49,7 +49,7 @@ def test_channel_details_user_not_in_channel(user_list, channel_id):
     with pytest.raises(AccessError):
         channel_details_v1(user_3, channel_id)
         
-def test_channel_details_one_owner(channel_id):
+def test_channel_details_one_owner(user_list, channel_id):
     user_1 = auth_login_v1("z5374603@ad.unsw.edu.au", "Ymc123")['auth_user_id']
     
     assert channel_details_v1(user_1, channel_id)['name'] == 'Channel_1'
@@ -145,7 +145,7 @@ def test_channel_details_two_join_members(user_list, channel_id):
         },
     ])
     
-def test_channel_details_invite_one_members(channel_id):
+def test_channel_details_invite_one_members(user_list,channel_id):
     user_1 = auth_login_v1("z5374603@ad.unsw.edu.au", "Ymc123")['auth_user_id']
     user_2 = auth_login_v1("z5201314@ad.unsw.edu.au", "Bojin123")['auth_user_id']
     channel_invite_v1(user_1, channel_id, user_2)
@@ -177,7 +177,7 @@ def test_channel_details_invite_one_members(channel_id):
         }
     ])
     
-def test_channel_details_invite_two_members(channel_id):  
+def test_channel_details_invite_two_members(user_list, channel_id):  
     user_1 = auth_login_v1("z5374603@ad.unsw.edu.au", "Ymc123")['auth_user_id']
     user_2 = auth_login_v1("z5201314@ad.unsw.edu.au", "Bojin123")['auth_user_id']
     user_3 = auth_login_v1("12345678@qq.com", "Cicy123")['auth_user_id']
