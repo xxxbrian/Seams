5371568f55f333d24c7cfb9cbce9b370c2be9254
Cicy Zhou <cicy.zhou7@gmail.com>
2022-03-03 16:53:25 +1100

    updated for messages object

--- src/data_store.py
+++ src/data_store.py
@@ -25,7 +25,9 @@ Example usage:
 '''
 
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
-initial_object = {'users': [], 'channels': []}
+initial_object = {'users': [], 'channels': [], 'messages': []}
+
+
 
 ## YOU SHOULD MODIFY THIS OBJECT ABOVE
 
--- tests/other_test.py
+++ tests/other_test.py
@@ -6,11 +6,12 @@ from src.data_store import data_store
 # adding user and channel and test if clear_v1 resets it
 def clear_v1_test():
     store = data_store.get()
-    users = store['users']      
-    chann = store['channels']
-    users.append("users_test")      
-    chann.append('chann_test')
+    store['users'].append("users_test")      
+    store['channels'].append('chann_test')
+    store['messages'].append("msg_test")
+
     clear_v1()
     assert store['users'] == []
     assert store['channels'] == []
+    assert store['messages'] == []
     
