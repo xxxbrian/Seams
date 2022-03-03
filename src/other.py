e1c80d5172ff66221f0c614619d2bc7f90011bc8
Cicy Zhou <cicy.zhou7@gmail.com>
2022-03-03 16:58:59 +1100

    updated functioning clear_v1

--- src/other.py
+++ src/other.py
@@ -1,7 +1,11 @@
 from src.data_store import data_store
 
+# resets all data
+# subject to change if dictionary keys expand in the initial object from data_store.py
+
 def clear_v1():
     store = data_store.get()
     store['users'] = []
     store['channels'] = []
+    store['messages'] = []
     data_store.set(store)
