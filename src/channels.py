cda208f784a6ee1649c301d1339d449bc1f806f6
Bojin Li <bojin.li@foxmail.com>
2022-03-04 18:00:27 +0800

    Completed channels create

--- src/channels.py
+++ src/channels.py
@@ -1,24 +1,30 @@
-def channels_list_v1(auth_user_id):      
+from src.error import InputError
+from src.type import User, Channel
+
+
+def channels_list_v1(auth_user_id):
     return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
+        'channels': [{
+            'channel_id': 1,
+            'name': 'My Channel',
+        }],
     }
+
 
 def channels_listall_v1(auth_user_id):
     return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
+        'channels': [{
+            'channel_id': 1,
+            'name': 'My Channel',
+        }],
     }
 
+
 def channels_create_v1(auth_user_id, name, is_public):
+    if Channel.check_name_invalid(name):
+        raise InputError
+    new_channel = Channel(auth_user_id, name, is_public)
+    new_channel.add_to_store()
     return {
-        'channel_id': 1,
+        'channel_id': new_channel.channel_id,
     }
