955f1911d00c150ec5de4e5d6ec944e7f4cb0851
Cicy Zhou <cicy.zhou7@gmail.com>
2022-03-05 16:58:38 +1100

    channel.py style complete

--- src/channel.py
+++ src/channel.py
@@ -3,6 +3,9 @@ from src.type import User, Channel
 
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
+    """Invites a user with ID u_id to join a channel with ID channel_id.
+    Once invited, the user is added to the channel immediately."""
+
     auth_user = User.find_by_id(auth_user_id)
     user = User.find_by_id(u_id)
     channel = Channel.find_by_id(channel_id)
@@ -20,6 +23,9 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
 
 
 def channel_details_v1(auth_user_id, channel_id):
+    """Given channel_id that the authorised user is a member of,
+    provide basic details about the channel."""
+
     user = User.find_by_id(auth_user_id)
     channel = Channel.find_by_id(channel_id)
     if channel is None:
@@ -33,7 +39,10 @@ def channel_details_v1(auth_user_id, channel_id):
 
 
 def channel_messages_v1(auth_user_id, channel_id, start):
-    user = User.find_by_id(auth_user_id)
+    """given a channel that the user is a part of,
+        return up to 50 messages between index
+        start" and "start + 50"""
+
     channel = Channel.find_by_id(channel_id)
     if channel is None:
         raise InputError
@@ -41,6 +50,8 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         raise InputError
     if not channel.has_user(User):
         raise AccessError
+
+    # Message with index 0 is the most recent message in the channel.
     end = start + 50 if start + 50 <= len(channel.messages) else -1
     msg_list = list(msg.todict() for msg in channel.get_message(start, end))
     return {
@@ -51,13 +62,18 @@ def channel_messages_v1(auth_user_id, channel_id, start):
 
 
 def channel_join_v1(auth_user_id, channel_id):
+    """Given a channel that the authorised user can join,
+    adds them to that channel."""
+
     channel = Channel.find_by_id(channel_id)
     user = User.find_by_id(auth_user_id)
+
     if channel is None:
         raise InputError
     if channel.has_user(user):
         raise InputError
     if not channel.is_public:
         raise AccessError
+
     channel.join(user)
     return {}
