ab304269ab393f546f81a1332681d2a4eeda79ae
Bojin Li <bojin.li@foxmail.com>
2022-03-14 21:58:00 +0800

    Init iter2 message.py

---
+++ src/message.py
@@ -0,0 +1,14 @@
+def message_send_v1(token, channel_id, message):
+    pass
+
+
+def message_edit_v1(token, message_id, message):
+    pass
+
+
+def message_remove_v1(token, message_id):
+    pass
+
+
+def message_senddm_v1(token, dm_id, message):
+    pass
