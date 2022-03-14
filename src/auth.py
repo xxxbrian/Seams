bd7462a377a8b899016b8e6184259168ecd925ab
Bojin Li <bojin.li@foxmail.com>
2022-03-14 21:00:03 +0800

    auth/login/v2

--- src/auth.py
+++ src/auth.py
@@ -1,19 +1,23 @@
 from src.error import InputError
 from src.type import User
 
-def auth_login_v1(email, password):
+
+def auth_login_v2(email, password):
     """Given a registered user's email and password,
     returns their `auth_user_id` value."""
     # email entered does not belong to a user
     if not User.check_email_been_used(email):
         raise InputError
     # password is not correct
-    if not User.match_email_password(email,password):
+    if not User.match_email_password(email, password):
         raise InputError
     user = User.find_by_email(email)
+    token = user.generat_token()
     return {
+        'token': token,
         'auth_user_id': user.u_id,
     }
+
 
 def auth_register_v1(email, password, name_first, name_last):
     """register new user if all inputs are valid"""
