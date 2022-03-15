d31977d5a835cf52b118bce727d6793c4c4ca0bb
Bojin Li <bojin.li@foxmail.com>
2022-03-15 16:24:59 +0800

    Completed mplement channels/v2

--- src/channels.py
+++ src/channels.py
@@ -3,14 +3,10 @@ from src.type import User, Channel
 
 
 def channels_list_v2(token):
-    pass
-
-
-def channels_list_v1(auth_user_id):
     """Invites a user with u_id to join a channel with channel_id.
-        Once invited, the user is added to the channel immediately."""
+    Once invited, the user is added to the channel immediately."""
 
-    user = User.find_by_id(auth_user_id)
+    user = User.find_by_token(token)
     if user is None:
         raise AccessError
 
@@ -27,14 +23,10 @@ def channels_list_v1(auth_user_id):
 
 
 def channels_listall_v2(token):
-    pass
-
-
-def channels_listall_v1(auth_user_id):
     """Provide a list of all channels, including private channels,
         and their associated details"""
 
-    if User.find_by_id(auth_user_id) is None:
+    if User.find_by_token(token) is None:
         raise AccessError
     channels_list = Channel.get_allchannel()
     info = []
@@ -47,18 +39,15 @@ def channels_listall_v1(auth_user_id):
 
 
 def channels_create_v2(token, name, is_public):
-    pass
-
-
-def channels_create_v1(auth_user_id, name, is_public):
     """Creates a new channel with 'name' (either public or private).
         The user who created it automatically joins the channel"""
 
-    if User.find_by_id(auth_user_id) is None:
+    user = User.find_by_token(token)
+    if user is None:
         raise AccessError
     if Channel.check_name_invalid(name):
         raise InputError
-    new_channel = Channel(auth_user_id, name, is_public)
+    new_channel = Channel(user.u_id, name, is_public)
     new_channel.add_to_store()
 
     return {
