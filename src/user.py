adce50ad4993b4b4627a8903e0d58592ad22de6e
Bojin Li <bojin.li@foxmail.com>
2022-03-16 20:00:35 +0800

    Added error descripiton

--- src/user.py
+++ src/user.py
@@ -5,28 +5,28 @@ from src.error import AccessError, InputError
 def users_all_v1(token):
     auth_user = User.find_by_token(token)
     if auth_user is None:
-        raise AccessError
+        raise AccessError(description='Permission denied')
     return {'users': [user.todict() for user in User.find_all()]}
 
 
 def user_profile_v1(token, u_id):
     auth_user = User.find_by_token(token)
     if auth_user is None:
-        raise AccessError
+        raise AccessError(description='Permission denied')
     user = User.find_by_id(u_id)
     if user is None:
-        raise InputError
+        raise InputError(description='User not exist')
     return {'user': user.todict()}
 
 
 def user_profile_setname_v1(token, name_first, name_last):
     user = User.find_by_token(token)
     if user is None:
-        raise AccessError
+        raise AccessError(description='Permission denied')
     if User.check_name_invalid(name_first):
-        raise InputError
+        raise InputError(description='Name invalid')
     if User.check_name_invalid(name_last):
-        raise InputError
+        raise InputError(description='Name invalid')
     user.name_first = name_first
     user.name_last = name_last
     return {}
@@ -35,11 +35,11 @@ def user_profile_setname_v1(token, name_first, name_last):
 def user_profile_setemail_v1(token, email):
     user = User.find_by_token(token)
     if user is None:
-        raise AccessError
+        raise AccessError(description='Permission denied')
     if User.check_email_invalid(email):
-        raise InputError
+        raise InputError(description='Email invalid')
     if User.check_email_been_used(email):
-        raise InputError
+        raise InputError(description='Email has been used')
     user.email = email
     return {}
 
@@ -47,12 +47,12 @@ def user_profile_setemail_v1(token, email):
 def user_profile_sethandle_v1(token, handle_str):
     user = User.find_by_token(token)
     if user is None:
-        raise AccessError
+        raise AccessError(description='Permission denied')
     if User.check_handle_length_invalid(handle_str):
-        raise InputError
+        raise InputError(description='Handle length invalid')
     if User.check_handle_content_invalid(handle_str):
-        raise InputError
+        raise InputError(description='Handle character invalid')
     if User.check_handle_been_used(handle_str):
-        raise InputError
+        raise InputError(description='Handle has been used')
     user.handle_str = handle_str
     return {}
