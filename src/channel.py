f006dacb55569d93f02a09a68e960a023b6ab22b
Bojin Li <bojin.li@foxmail.com>
2022-03-16 21:34:36 +0800

    v1 -> v2

--- src/channel.py
+++ src/channel.py
@@ -3,16 +3,14 @@ from src.type import User, Channel
 
 
 def channel_invite_v2(token, channel_id, u_id):
-    pass
-
-
-def channel_invite_v1(auth_user_id, channel_id, u_id):
     """Invites a user with ID u_id to join a channel with ID channel_id.
     Once invited, the user is added to the channel immediately."""
 
-    auth_user = User.find_by_id(auth_user_id)
+    auth_user = User.find_by_token(token)
     user = User.find_by_id(u_id)
     channel = Channel.find_by_id(channel_id)
+    if auth_user is None:
+        raise AccessError
     if channel is None:
         raise InputError
     if user is None:
@@ -27,15 +25,13 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
 
 
 def channel_details_v2(token, channel_id):
-    pass
-
-
-def channel_details_v1(auth_user_id, channel_id):
     """Given channel_id that the authorised user is a member of,
     provide basic details about the channel."""
 
-    user = User.find_by_id(auth_user_id)
+    user = User.find_by_token(token)
     channel = Channel.find_by_id(channel_id)
+    if user is None:
+        raise AccessError
     if channel is None:
         raise InputError
     if not channel.has_user(user):
@@ -47,16 +43,14 @@ def channel_details_v1(auth_user_id, channel_id):
 
 
 def channel_messages_v2(token, channel_id, start):
-    pass
-
-
-def channel_messages_v1(auth_user_id, channel_id, start):
     """given a channel that the user is a part of,
         return up to 50 messages between index
         start" and "start + 50"""
 
-    user = User.find_by_id(auth_user_id)
+    user = User.find_by_token(token)
     channel = Channel.find_by_id(channel_id)
+    if user is None:
+        raise AccessError
     if channel is None:
         raise InputError
     if not channel.has_user(user):
@@ -75,15 +69,11 @@ def channel_messages_v1(auth_user_id, channel_id, start):
 
 
 def channel_join_v2(token, channel_id):
-    pass
-
-
-def channel_join_v1(auth_user_id, channel_id):
     """Given a channel that the authorised user can join,
     adds them to that channel."""
 
-    channel = Channel.find_by_id(channel_id)
-    user = User.find_by_id(auth_user_id)
+    user = User.find_by_token(token)
+    channel = Channel.find_by_(channel_id)
 
     if user is None:
         raise AccessError
@@ -91,7 +81,7 @@ def channel_join_v1(auth_user_id, channel_id):
         raise InputError
     if channel.has_user(user):
         raise InputError
-    if not channel.is_public and auth_user_id != 0:
+    if not channel.is_public and user.u_id != 0:
         raise AccessError
 
     channel.join(user)
