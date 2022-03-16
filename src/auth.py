d4de7c6e1640277469c011034a316c0a8a24639a
Bojin Li <bojin.li@foxmail.com>
2022-03-16 21:18:35 +0800

    Added error descripiton

--- src/auth.py
+++ src/auth.py
@@ -7,10 +7,10 @@ def auth_login_v2(email, password):
     returns their `auth_user_id` value."""
     # email entered does not belong to a user
     if not User.check_email_been_used(email):
-        raise InputError
+        raise InputError(description='Account not found')
     # password is not correct
     if not User.match_email_password(email, password):
-        raise InputError
+        raise InputError(description='Incorrect password')
     user = User.find_by_email(email)
     token = user.generat_token()
     return {
@@ -23,15 +23,15 @@ def auth_register_v2(email, password, name_first, name_last):
     """register new user if all inputs are valid"""
 
     if User.check_email_invalid(email):
-        raise InputError
+        raise InputError(description='Email invalid')
     if User.check_email_been_used(email):
-        raise InputError
+        raise InputError(description='Email has been used')
     if User.check_password_invalid(password):
-        raise InputError
+        raise InputError(description='Password invalid')
     if User.check_name_invalid(name_first):
-        raise InputError
+        raise InputError(description='Name invalid')
     if User.check_name_invalid(name_last):
-        raise InputError
+        raise InputError(description='Name invalid')
 
     new_user = User(email, password, name_first, name_last)
     new_user.add_to_store()
@@ -43,4 +43,4 @@ def auth_logout_v1(token):
     if User.token_in_store(token):
         User.remove_token(token)
     else:
-        raise InputError
+        raise InputError(description='Token invalid')
