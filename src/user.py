3e6d5466838dcb7ab23a7a15409ad752232ee51a
Bojin Li <bojin.li@foxmail.com>
2022-03-16 19:07:11 +0800

    Completed user_profile_v1

--- src/user.py
+++ src/user.py
@@ -3,14 +3,20 @@ from src.error import AccessError, InputError
 
 
 def users_all_v1(token):
-    user = User.find_by_token(token)
-    if user is None:
+    auth_user = User.find_by_token(token)
+    if auth_user is None:
         raise AccessError
     return {'users': [user.todict() for user in User.find_all()]}
 
 
 def user_profile_v1(token, u_id):
-    pass
+    auth_user = User.find_by_token(token)
+    if auth_user is None:
+        raise AccessError
+    user = User.find_by_id(u_id)
+    if user is None:
+        raise InputError
+    return {'user': user.todict()}
 
 
 def user_profile_setname_v1(token, name_first, name_last):
