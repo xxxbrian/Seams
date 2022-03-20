af06a7688c372fe58665af74a1d3016d59d3ee71
Steve <z5374603@ad.unsw.edu.au>
2022-03-20 14:28:11 +0800

    rewrite the dm_create tests

--- tests/test_http/test_dm_create_v1.py
+++ tests/test_http/test_dm_create_v1.py
@@ -1,12 +1,13 @@
 """
 u_ids contains the user(s) that this DM is directed to, and will not include the creator. The creator is the owner of the DM. name should be automatically generated based on the users that are in this DM. The name should be an alphabetically-sorted, comma-and-space-separated list of user handles, e.g. 'ahandle1, bhandle2, chandle3'.
 """
-
+import random
+from urllib import response
 import pytest
 import requests
 import json
 from src.config import url
-from src.error import InputError
+from src.error import AccessError, InputError
 
 @pytest.fixture(name = 'user_list')
 def create_user_list():
@@ -40,61 +41,108 @@ def create_user_list():
                                             'name_last': 'Zhou'}))
     return user_list
 
-@pytest.fixture(name = 'info')
-def one_user_create_dm(user_list):
-    dict_list = user_list
-    """User01 logs in"""
-    r = requests.post(url + 'auth/login/v2', 
-                    json = {'email': 'z5374603@unsw.com',
-                            'password': '123456',})
-    payload = r.json()
-    token = payload['token']
-    auth_user_id = payload['auth_user_id']
-    # User01 create a dm
-    u_id = []
-    u_id.append(dict_list[1].json()['auth_user_id'])
-    u_id.append(dict_list[2].json()['auth_user_id'])
-    u_id.append(dict_list[3].json()['auth_user_id'])
-    r = requests.post(url + 'dm/create/v1', 
-                    json = {'token': token,
-                            'u_ids': u_id,})
-    payload = r.json()
-    dm_id = payload['dm_id']
-    return {
-        'token': token,
-        'u_id': auth_user_id,
-        'dm_id': dm_id,
-    }
+@pytest.fixture(name = 'login_list')
+def login_users():
+    '''
+    
+    This function is to pre-login 4 users for further tests
+    
+    returns:
+        login_list, contains 4 pre-register users' information
+        
+    '''
+    login_list = []
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374603@unsw.com",
+                                            "password": "123456"}).json())
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374602@unsw.com",
+                                            "password": "123456"}).json())
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374601@unsw.com",
+                                            "password": "123456"}).json())
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374600@unsw.com",
+                                            "password": "123456"}).json())
+    return login_list
 
-def test_dm_create_invalid_uid(info):
+def test_dm_create_successfully(user_list, login_list):
+    '''
+    
+    This test is for testing create a dm successfully
+    
+    Parameters:
+        user_list, login_list
+        
+    Return:
+        N/A
+        
+    '''
+    response_1 = requests.post(f'{url}dm/create/v1',
+                               json = {'token': login_list[0]['token'],
+                                       'u_ids': [login_list[1]['auth_user_id'], login_list[2]['auth_user_id']],})
+    assert response_1.status_code == 200
+
+def test_dm_create_invalid_uid(user_list, login_list):
     """
         Test invalid user id
+        
         Raises:
             InputError
     """
-    res = requests.post(url + 'dm/create/v1',
-                        json = {'token': info['token'],
-                                'u_ids': [-5, -80],})
-    assert res.status_code == InputError.code
+    invalid_u_ids = []
+    while(len(invalid_u_ids)<3):
+        new_id = random.random()
+        if not new_id in [login_list[i]['auth_user_id'] for i in range(0,3)]:
+            invalid_u_ids.append(new_id)
+    response_1 = requests.post(url + "dm/create/v1",
+                               json = {'token': login_list[0]['token'],
+                                       'u_ids': invalid_u_ids})
+    assert response_1.status_code == InputError.code
 
-def test_dm_create_multiple_uid(info):
+def test_dm_create_duplicated_uid(user_list, login_list):
     """
+    
         Test duplicated user id
+        
         Raises:
             InputError
+            
     """
-
-    res = requests.post(url + 'dm/create/v1',
-                        json = {'token': info['token'],
-                                'u_ids': [1,1,1],})
-    payload = res.json()
-    temp = -999
-    is_duplicate = False
-
-    for i in payload['u_ids']:
-        if i == temp:
-            is_duplicate = True
-        temp = i
-
-    if is_duplicate is True:
-        assert res.status_code == InputError.code
+    response_1 = requests.post(url + "dm/create/v1",
+                               json = {'token': login_list[0]['token'],
+                                       'u_ids': [login_list[1]['auth_user_id'], login_list[1]['auth_user_id']]})
+    assert response_1.status_code == InputError.code
+    
+def test_dm_create_invalid_token(user_list, login_list):
+    """
+    
+        Test invalid token
+        
+        Raises:
+            AccessError
+            
+    """
+    # normal situation
+    response_1 = requests.post(f'{url}dm/create/v1',
+                               json = {'token': -1,
+                                       'u_ids': [login_list[1]['auth_user_id'], login_list[2]['auth_user_id']],})
+    assert response_1.status_code == AccessError.code
+    
+    # invalid_u_id
+    invalid_u_ids = []
+    while(len(invalid_u_ids)<3):
+        new_id = random.random()
+        if not new_id in [login_list[i]['auth_user_id'] for i in range(0,3)]:
+            invalid_u_ids.append(new_id)
+    response_1 = requests.post(url + "dm/create/v1",
+                               json = {'token': -1,
+                                       'u_ids': invalid_u_ids})
+    assert response_1.status_code == AccessError.code
+    
+    # duplicated uid
+    response_1 = requests.post(url + "dm/create/v1",
+                               json = {'token': -1,
+                                       'u_ids': [login_list[1]['auth_user_id'], login_list[1]['auth_user_id']]})
+    assert response_1.status_code == AccessError.code
+    
