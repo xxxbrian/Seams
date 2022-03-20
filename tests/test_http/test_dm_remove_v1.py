c1d2d43315a5bbfcb1083cc1dc8a2d032f1234b7
Steve <z5374603@ad.unsw.edu.au>
2022-03-20 17:30:42 +0800

    Rewrite test_dm_remove_v1

--- tests/test_http/test_dm_remove_v1.py
+++ tests/test_http/test_dm_remove_v1.py
@@ -1,6 +1,8 @@
+from urllib import response
 import pytest
 import requests
 import json
+import random
 from src.config import url
 from src.error import InputError, AccessError
 
@@ -36,83 +38,161 @@ def create_user_list():
                                             'name_last': 'Zhou'}))
     return user_list
 
-@pytest.fixture(name = 'info')
-def one_user_create_dm(user_list):
-    dict_list = user_list
-    """User01 logs in"""
-    r = requests.post(url + 'auth/login/v2', 
-                    json = {'email': 'z5374603@unsw.com',
-                            'password': '123456',})
-    payload = r.json()
-    token = payload['token']
-    auth_user_id = payload['auth_user_id']
-    # User01 create a dm
-    u_id = []
-    u_id.append(dict_list[1].json()['auth_user_id'])
-    u_id.append(dict_list[2].json()['auth_user_id'])
-    u_id.append(dict_list[3],json()['auth_user_id'])
-    r = requests.post(url + 'dm/create/v1', 
-                    json = {'token': token,
-                            'u_ids': u_id,})
-    payload = r.json()
-    dm_id = payload['dm_id']
-    return {
-        'token': token,
-        'u_id': auth_user_id,
-        'dm_id': dm_id,
-    }
-
-@pytest.fixture(name="dm1")
-def users_setup():
-    """User1 creates a dm and it directes to user4 and store the dm_id and name"""
-    resp = requests.post(url + 'auth/login/v2',
-                        json = {'email': 'z5374603@unsw.com', 
-                                'password': '123456'})
-    user1 = resp.json()
+@pytest.fixture(name = 'login_list')
+def login_users():
+    '''
     
-    resp = requests.post(url + 'auth/login/v2',
-                        json = {"email": "z5374602@unsw.com",
-                                "password": "123456",})
-    user4 = resp.json()
+    This function is to pre-login 4 users for further tests
     
-    u_ids = []
-    u_ids.append(user4["auth_user_id"])
-    resp = requests.post(url + 'dm/create/v1',
-                        json = {"token": user1["token"],
-                                'u_ids': u_ids,})
-    requests.post(url + 'auth/logout/v1', json={'token': user1['token']})
-    requests.post(url + 'auth/logout/v1', json={'token': user4['token']})
-    dm1 = resp.json()
+    returns:
+        login_list, contains 4 pre-register users' information
+        
+    '''
+    login_list = []
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374603@unsw.com",
+                                            "password": "123456"}).json())
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374602@unsw.com",
+                                            "password": "123456"}).json())
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374601@unsw.com",
+                                            "password": "123456"}).json())
+    login_list.append(requests.post(url + "auth/login/v2",
+                                    json = {"email": "z5374600@unsw.com",
+                                            "password": "123456"}).json())
+    return login_list
 
-    return dm1
+@pytest.fixture(name = 'dm_list')
+def create_dm(login_list):
+    '''
+    This function is to pre-create some dms for further tests
+    
+    Return:
+        dm_list, contain dm_id and name
+    '''
+    dm_list = []
+    # dm_list[0]: user[0], user[1], user[2]
+    dm_list.append(requests.post(url + "dm/create/v1",
+                                 json = {'token': login_list[0]['token'],
+                                         'u_ids': [login_list[1]['auth_user_id'], login_list[2]['auth_user_id']]}).json())
+    # dm_list[1]: user[0], user[2]. user[3]
+    dm_list.append(requests.post(url + "dm/create/v1",
+                                 json = {'token': login_list[0]['token'],
+                                         'u_ids': [login_list[3]['auth_user_id'], login_list[2]['auth_user_id']]}).json())
+    
+    # dm_list[2]: user[1], user[2]. user[3]
+    dm_list.append(requests.post(url + "dm/create/v1",
+                                 json = {'token': login_list[1]['token'],
+                                         'u_ids': [login_list[2]['auth_user_id'], login_list[3]['auth_user_id']]}).json())
+    
+    return dm_list
 
-def test_invalid_dm_id(user_list):
-    """Test dm_remove_v1, it will raise Input error 
-    if give a invalid dm ID."""
-    res = requests.delete(url + 'dm/remove/v1',
-                            json = {"token": user_list[0]["token"], 
-                                    "dm_id": -1})
-    assert res.status_code == InputError.code
 
-def test_author_not_creator(user_list, dm1):
-    """Test dm/remove/v1 when the user is 
-    not the original DM creator 
-    then raise AccessError."""
+def test_dm_remove_normal(user_list, login_list, dm_list):
+    '''
+    
+    Test remove successfully
+    
+    Parameters:
+        user_list, login_list, dm_list
+        
+    Assumption:
+        dm/list/v1 is working well
 
-    resp = requests.delete(url + 'dm/remove/v1',
-                            json = {"token": user_list[3].json()["token"], 
-                                    "dm_id": dm1["dm_id"]})
-    assert resp.status_code == AccessError.code
-
-def test_remove_success(user_list, dm1):
-    """
-    User1 creates a dm and it directes to user4
-    User1 remove dm1 successfully.
-    """ 
+    '''
+    dm_1 = 'steveyang cicyzhou bojinli'
     requests.delete(url + 'dm/remove/v1',
-                    json = {"token": user_list[0].json()["token"], 
-                            "dm_id": dm1["dm_id"]})
+                    json = {"token": login_list[0]["token"], 
+                            "dm_id": dm_list[0]['dm_id']})
+    response_1 = requests.get(url + "dm/list/v1",
+                              params = {'token': login_list[0]['token']}).json()
+    # remove dm[0]
+    assert response_1 == {
+        'dms':[
+        {
+            'dm_id': dm_list[1]['dm_id'],
+            'name': dm_1
+        },
+    ]}
 
-    resp = requests.get(url + 'dm/list/v1',
-                        params = {"token": user_list[0].json()["token"]}) 
-    assert json.loads(resp.text) == {"dms": []}
+def test_dm_remove_invalid_dm_id(user_list, login_list, dm_list):
+    """
+    
+    Test dm_remove_v1 input a invalid dm ID.
+    
+    Parameters:
+        user_list, login_list, dm_list
+    
+    Raises:
+        InputError
+        
+    """
+    new_id = random.randint(-65535, 65535)
+    if not new_id in [dm_list[i]['dm_id'] for i in range(0,3)]:
+        invalid_dm_id = new_id
+    response_1 = requests.delete(url + 'dm/remove/v1',
+                                 json = {"token": login_list[0]["token"], 
+                                         "dm_id": invalid_dm_id})
+    assert response_1.status_code == InputError.code
+
+def test_dm_remove_author_not_creator(user_list, login_list, dm_list):
+    """
+    
+    Test dm/remove/v1 when the user is not the original DM creator 
+    
+    Raise:
+        AccessError
+        
+    """
+
+    response_1 = requests.delete(url + 'dm/remove/v1',
+                                 json = {"token": login_list[3]["token"], 
+                                         "dm_id": dm_list[1]["dm_id"]})
+    assert response_1.status_code == AccessError.code
+
+def test_dm_remove_creater_no_longer_in_dm(user_list, login_list, dm_list):
+    '''
+    
+    Test when creater leave the dm
+    
+    Raises:
+        AccessError
+        
+    Assumption:
+        dm/leave/v1 is working well
+        
+    '''
+    # user[1] leaves the dm[2]
+    requests.post(url + "dm/leave/v1",
+                  json = {'token': login_list[1]['token'],
+                          'dm_id': dm_list[2]['dm_id']})
+    response_1 = requests.delete(url + 'dm/remove/v1',
+                                 json = {"token": login_list[1]["token"], 
+                                         "dm_id": dm_list[2]["dm_id"]})
+    assert response_1.status_code == AccessError.code
+    
+def test_dm_remove_creater_invalid_token(user_list, login_list, dm_list):
+    '''
+    
+    This test is to test when token is invalid 
+    
+    Raises:
+        AccessError
+        
+    '''
+    # normal
+    response_1 = requests.delete(url + 'dm/remove/v1',
+                                 json = {"token": -1, 
+                                         "dm_id": dm_list[0]['dm_id']})
+    assert response_1.status_code == AccessError.code
+    
+    # Invalid dm ID
+    new_id = random.randint(-65535, 65535)
+    if not new_id in [dm_list[i]['dm_id'] for i in range(0,3)]:
+        invalid_dm_id = new_id
+    response_2 = requests.delete(url + 'dm/remove/v1',
+                                 json = {"token": -1, 
+                                         "dm_id": invalid_dm_id})
+    assert response_2.status_code == AccessError.code
+    
