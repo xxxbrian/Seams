486e5c340b8c713795ecd3eb5ebe34abbd92b8d2
Steve <z5374603@ad.unsw.edu.au>
2022-04-06 21:17:56 +0800

    fix some error in tests and find an error in implement

--- tests/test_http/test_http_other.py
+++ tests/test_http/test_http_other.py
@@ -1,41 +1,10 @@
+import re
 import pytest
 import requests
 import json
 import random
 from src.config import url
 from src.error import InputError, AccessError
-
-@pytest.fixture(name = 'user_list')
-def create_user_list():
-    '''
-    This function is to pre-register 4 users for further tests
-    
-    returns:
-    user_list (dictionary), contains 4 pre-register users' information
-    '''
-    requests.delete(f"{url}clear/v1", json = {})    # clear all info in server
-    user_list = []
-    user_list.append(requests.post(f"{url}auth/register/v2",
-                                   json = { 'email': 'z5374603@unsw.com',
-                                            'password': '123456',
-                                            'name_first': 'Steve',
-                                            'name_last': 'Yang'}))
-    user_list.append(requests.post(f"{url}auth/register/v2",
-                                   json = { 'email': 'z5374602@unsw.com',
-                                            'password': '123456',
-                                            'name_first': 'Brian',
-                                            'name_last': 'Lee'}))
-    user_list.append(requests.post(f"{url}auth/register/v2",
-                                   json = { 'email': 'z5374601@unsw.com',
-                                            'password': '123456',
-                                            'name_first': 'Bojin',
-                                            'name_last': 'Li'}))
-    user_list.append(requests.post(f"{url}auth/register/v2",
-                                   json = {  'email': 'z5374600@unsw.com',
-                                            'password': '123456',
-                                            'name_first':'Cicy',
-                                            'name_last': 'Zhou'}))
-    return user_list
 
 @pytest.fixture(name = 'login_list')
 def login_users():
@@ -48,6 +17,27 @@ def login_users():
         
     '''
     login_list = []
+    requests.delete(f"{url}clear/v1", json = {})    # clear all info in server
+    requests.post(f"{url}auth/register/v2",
+                                   json = { 'email': 'z5374603@unsw.com',
+                                            'password': '123456',
+                                            'name_first': 'Steve',
+                                            'name_last': 'Yang'})
+    requests.post(f"{url}auth/register/v2",
+                                   json = { 'email': 'z5374602@unsw.com',
+                                            'password': '123456',
+                                            'name_first': 'Brian',
+                                            'name_last': 'Lee'})
+    requests.post(f"{url}auth/register/v2",
+                                   json = { 'email': 'z5374601@unsw.com',
+                                            'password': '123456',
+                                            'name_first': 'Bojin',
+                                            'name_last': 'Li'})
+    requests.post(f"{url}auth/register/v2",
+                                   json = {  'email': 'z5374600@unsw.com',
+                                            'password': '123456',
+                                            'name_first':'Cicy',
+                                            'name_last': 'Zhou'})
     login_list.append(requests.post(url + "auth/login/v2",
                                     json = {"email": "z5374603@unsw.com",
                                             "password": "123456"}).json())
@@ -147,25 +137,24 @@ def test_notification_in_channel(login_list, channel_list):
                          'u_id': login_list[1]['auth_user_id']})
     response_1 = requests.get(url + 'notifications/get/v1',
                               params={'token': login_list[1]['token']}).json()
-    assert response_1['notifications'][1]['channel_id'] == channel_list[0]['channel_id']
-    assert response_1['notifications'][1]['dm-id'] == -1
-    assert response_1['notifications'][1]['notification_message'] == "steveyang added you to Steve's channel"
-    # Because user[1] has been added to DM[0] at fixture, now the notification should be number 1
+    assert response_1['notifications'][0]['channel_id'] == channel_list[0]['channel_id']
+    assert response_1['notifications'][0]['dm_id'] == -1
+    assert response_1['notifications'][0]['notification_message'] == "steveyang added you to Steve's channel"
     
     # user[1] send a message in channel[0]
     response_2 = requests.post(url + 'message/send/v1',
                                json = {'token': login_list[1]['token'],
                                        'channel_id': channel_list[0]['channel_id'],
-                                       'message': 'Hello guys'})
+                                       'message': 'Hello guys'}).json()
     requests.post(url + "message/react/v1",
                   json = {'token': login_list[0]['token'],
                           'message_id': response_2['message_id'],
                           'react_id': 1})
     response_3 = requests.get(url + 'notifications/get/v1',
                               params={'token': login_list[1]['token']}).json()
-    assert response_3['notifications'][2]['channel_id'] == channel_list[0]['channel_id']
-    assert response_3['notifications'][2]['dm-id'] == -1
-    assert response_3['notifications'][2]['notification_message'] == "steveyang reacted to your message in Steve's channel"
+    assert response_3['notifications'][1]['channel_id'] == channel_list[0]['channel_id']
+    assert response_3['notifications'][1]['dm-id'] == -1
+    assert response_3['notifications'][1]['notification_message'] == "steveyang reacted to your message in Steve's channel"
 
 def test_notification_in_DM(login_list, dm_list):
     '''
@@ -184,14 +173,14 @@ def test_notification_in_DM(login_list, dm_list):
     response_1 = requests.get(url + 'notifications/get/v1',
                               params={'token': login_list[1]['token']}).json()
     assert response_1['notifications'][0]['channel_id'] == -1
-    assert response_1['notifications'][0]['dm-id'] == dm_list[0]['dm_id']
+    assert response_1['notifications'][0]['dm_id'] == dm_list[0]['dm_id']
     assert response_1['notifications'][0]['notification_message'] == "steveyang added you to bojinli, brianlee, steveyang"
     
     # user[1] sends a message in dm[0]
     response_2 = requests.post(url + 'message/senddm/v1',
                                json = {'token': login_list[1]['token'],
                                        'dm_id': dm_list[0]['dm_id'],
-                                       'message': 'Hello guys'})
+                                       'message': 'Hello guys'}).json()
     # user[0] reacts to this message
     requests.post(url + "message/react/v1",
                   json = {'token': login_list[0]['token'],
@@ -215,12 +204,19 @@ def test_search_normal(login_list, dm_list, channel_list):
         message/send/v1 is working well
     
     '''
-    # 3 users send the same messages in their channels for further tests
-    for i in range(3):
-        requests.post(url + 'message/send/v1',
-                      json = {'token': login_list[i]['token'],
-                              'channel_id': channel_list[i]['channel_id'],
-                              'message': 'Hello guys'})
+    # user[0] send 'Hello guys' 3 times
+    requests.post(url + 'message/send/v1',
+                  json = {'token': login_list[0]['token'],
+                          'channel_id': channel_list[0]['channel_id'],
+                          'message': 'Hello guys'})
+    requests.post(url + 'message/send/v1',
+                  json = {'token': login_list[0]['token'],
+                          'channel_id': channel_list[0]['channel_id'],
+                          'message': 'Hello guys'})
+    requests.post(url + 'message/send/v1',
+                  json = {'token': login_list[0]['token'],
+                          'channel_id': channel_list[0]['channel_id'],
+                          'message': 'Hello guys'})
     
     requests.post(url + 'message/senddm/v1',
                   json = {'token': login_list[1]['token'],
@@ -235,18 +231,18 @@ def test_search_normal(login_list, dm_list, channel_list):
                           'dm_id': dm_list[0]['dm_id'],
                           'message': 'Hello world'})
     
-    response_1 = requests.post(url + 'message/send/v1',
-                               json = {'token': login_list[0],
+    response_1 = requests.get(url + 'search/v1',
+                               params = {'token': login_list[0]['token'],
                                        'query_str': 'guys'}).json()
     # assert 3 messages about 'guys'
     assert len(response_1['messages']) == 3
-    response_2 = requests.post(url + 'message/send/v1',
-                               json = {'token': login_list[0],
+    response_2 = requests.get(url + 'search/v1',
+                              params = {'token': login_list[0]['token'],
                                        'query_str': 'h'}).json()
     # assert 6 messages about 'h', also, this test is to test case-insensitive
     assert len(response_2['messages']) == 6
-    response_3 = requests.post(url + 'message/send/v1',
-                               json = {'token': login_list[0],
+    response_3 = requests.get(url + 'search/v1',
+                              params = {'token': login_list[0]['token'],
                                        'query_str': 'steve'}).json()
     # assert 1 messages about 'steve', also, this test is to test case-insensitive
     assert len(response_3['messages']) == 1
@@ -267,12 +263,19 @@ def test_search_invalid_query_string(login_list, dm_list, channel_list):
     while len(too_long_str) <= 1000:
         too_long_str += 'a'
         
-    # 3 users send the same messages in their channels for further tests
-    for i in range(3):
-        requests.post(url + 'message/send/v1',
-                      json = {'token': login_list[i]['token'],
-                              'channel_id': channel_list[i]['channel_id'],
-                              'message': 'Hello guys'})
+    # user[0] send 'Hello guys' 3 times
+    requests.post(url + 'message/send/v1',
+                  json = {'token': login_list[0]['token'],
+                          'channel_id': channel_list[0]['channel_id'],
+                          'message': 'Hello guys'})
+    requests.post(url + 'message/send/v1',
+                  json = {'token': login_list[0]['token'],
+                          'channel_id': channel_list[0]['channel_id'],
+                          'message': 'Hello guys'})
+    requests.post(url + 'message/send/v1',
+                  json = {'token': login_list[0]['token'],
+                          'channel_id': channel_list[0]['channel_id'],
+                          'message': 'Hello guys'})
     
     requests.post(url + 'message/senddm/v1',
                   json = {'token': login_list[1]['token'],
@@ -287,14 +290,14 @@ def test_search_invalid_query_string(login_list, dm_list, channel_list):
                           'dm_id': dm_list[0]['dm_id'],
                           'message': 'Hello world'})
     # test for empty query_str
-    response_1 = requests.post(url + 'message/send/v1',
-                               json = {'token': login_list[0],
+    response_1 = requests.get(url + 'search/v1',
+                              params = {'token': login_list[0]['token'],
                                        'query_str': ''})
     assert response_1.status_code == InputError.code
     
     # test for too long query_str
-    response_2 = requests.post(url + 'message/send/v1',
-                               json = {'token': login_list[0],
+    response_2 = requests.get(url + 'search/v1',
+                              params = {'token': login_list[0]['token'],
                                        'query_str': too_long_str})
     assert response_2.status_code == InputError.code
     
@@ -304,7 +307,7 @@ def test_search_invalid_token():
     This test is to test when token is invalid
     
     '''
-    response_1 = requests.post(url + 'message/send/v1',
-                               json = {'token': -1,
+    response_1 = requests.get(url + 'search/v1',
+                              params = {'token': -1,
                                        'query_str': 'haha'})
     assert response_1.status_code == AccessError.code
