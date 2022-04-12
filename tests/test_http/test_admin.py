ff19a166e8e46901cd2145d9a4feeb409d081876
Steve <z5374603@ad.unsw.edu.au>
2022-04-12 16:05:00 +0800

    improve test in admin for iter3

--- tests/test_http/test_admin.py
+++ tests/test_http/test_admin.py
@@ -160,7 +160,7 @@ def test_admin_user_remove_normal(user_list, login_list, channel_list, dm_list):
             'handle_str': 'cicyzhou'
         }
     ]
-    remove_user = {
+    removed_user = {
             'u_id': login_list[1]['auth_user_id'],
             'email': 'z5374602@unsw.com',
             'name_first': 'Removed',
@@ -221,60 +221,53 @@ def test_admin_user_remove_normal(user_list, login_list, channel_list, dm_list):
     response_9 = requests.post(url + 'auth/logout/v1',
                                json={'token': login_list[1]['token']})
     
-    assert response_1 == {
-        'name': "Brian's channel",
-        'is_public': True,
-        'owner_members':[{
-            'u_id': login_list[0]['auth_user_id'],
-            'email': "z5374603@unsw.com",
-            'name_first': 'Steve',
-            'name_last': 'Yang',
-            'handle_str': 'steveyang'
-        }],
-        'all_members': [{
-            'u_id': login_list[0]['auth_user_id'],
-            'email': "z5374603@unsw.com",
-            'name_first': 'Steve',
-            'name_last': 'Yang',
-            'handle_str': 'steveyang'
-        }],
-    }
+    assert response_1['name'] == "Brian's channel"
+    assert response_1['is_public'] == True
+    assert response_1['owner_members'][0]['u_id'] == login_list[0]['auth_user_id']
+    assert response_1['owner_members'][0]['email'] == "z5374603@unsw.com"
+    assert response_1['owner_members'][0]['name_first'] == 'Steve'
+    assert response_1['owner_members'][0]['name_last'] == 'Yang'
+    assert response_1['owner_members'][0]['handle_str'] == 'steveyang'
+    assert response_1['all_members'][0]['u_id'] == login_list[0]['auth_user_id']
+    assert response_1['all_members'][0]['email'] == "z5374603@unsw.com"
+    assert response_1['all_members'][0]['name_first'] == 'Steve'
+    assert response_1['all_members'][0]['name_last'] == 'Yang'
+    assert response_1['all_members'][0]['handle_str'] == 'steveyang'
     
     dm_0 = 'bojinli, brianlee, steveyang'
-    assert response_2 == {
-        'name': dm_0,
-        'members':
-            [
-                {
-                    'u_id': login_list[2]['auth_user_id'],
-                    'email': "z5374601@unsw.com",
-                    'name_first': 'Bojin',
-                    'name_last': 'Li',
-                    'handle_str': 'bojinli'
-                },
-                {
-                    'u_id': login_list[0]['auth_user_id'],
-                    'email': "z5374603@unsw.com",
-                    'name_first': 'Steve',
-                    'name_last': 'Yang',
-                    'handle_str': 'steveyang'
-                },
-            ]
-    }
+    assert response_2['name'] == dm_0
+    assert response_2['members'][0]['u_id'] == login_list[2]['auth_user_id']
+    assert response_2['members'][0]['email'] == "z5374601@unsw.com"
+    assert response_2['members'][0]['name_first'] == 'Bojin'
+    assert response_2['members'][0]['name_last'] == 'Li'
+    assert response_2['members'][0]['handle_str'] == 'bojinli'
+    assert response_2['members'][1]['u_id'] == login_list[0]['auth_user_id']
+    assert response_2['members'][1]['email'] == "z5374603@unsw.com"
+    assert response_2['members'][1]['name_first'] == 'Steve'
+    assert response_2['members'][1]['name_last'] == 'Yang'
+    assert response_2['members'][1]['handle_str'] == 'steveyang'
     
     # assert user[1] is not in users all
-    assert users == response_3['users']
+    for i in range(3):
+        assert users[i]['u_id'] == response_3['users'][i]['u_id']
+        assert users[i]['email'] == response_3['users'][i]['email']
+        assert users[i]['name_first'] == response_3['users'][i]['name_first']
+        assert users[i]['name_last'] == response_3['users'][i]['name_last']
+        assert users[i]['handle_str'] == response_3['users'][i]['handle_str']
     assert response_4['messages'][0]['message'] == 'Removed user'
     assert response_5['messages'][0]['message'] == 'Removed user'
-    assert response_6['user'] == remove_user
+    assert removed_user['u_id'] == response_6['user']['u_id']
+    assert removed_user['email'] == response_6['user']['email']
+    assert removed_user['name_first'] == response_6['user']['name_first']
+    assert removed_user['name_last'] == response_6['user']['name_last']
+    assert removed_user['handle_str'] == response_6['user']['handle_str']
     assert response_7.status_code == 200
-    assert response_8['user'] == {
-            'u_id': response_7.json()['auth_user_id'],
-            'email': 'z5374602@unsw.com',
-            'name_first': 'Brian',
-            'name_last': 'Lee',
-            'handle_str': 'brianlee'
-        }
+    
+    assert response_8['user']['u_id'] == response_7.json()['auth_user_id']
+    assert response_8['user']['email'] == 'z5374602@unsw.com'
+    assert response_8['user']['name_first'] == 'Brian'
+    assert response_8['user']['name_last'] == 'Lee'
+    assert response_8['user']['handle_str'] == 'brianlee'
     assert response_9.status_code == AccessError.code
     
 def test_admin_user_remove_invalid_u_id(user_list, login_list):
