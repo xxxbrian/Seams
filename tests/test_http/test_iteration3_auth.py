450ee5033b8ffefb2fac48c32dfdd6787d416db3
Steve <z5374603@ad.unsw.edu.au>
2022-04-17 23:52:12 +0800

    add 2 more tests for reset password

--- tests/test_http/test_iteration3_auth.py
+++ tests/test_http/test_iteration3_auth.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from src.config import url
+from src.config import SECRET, url 
 from src.error import InputError
 
 def test_auth_passwordreset_request_normal():
@@ -27,6 +27,44 @@ def test_auth_passwordreset_request_not_user():
     res = requests.post(url + "auth/passwordreset/request/v1",
                         json = {'email': '1981@qq.com'})
     assert res.status_code == 200
+    
+def test_auth_passwordreset_reset_correct_reset_code():
+    '''
+    
+    This test is to test when reset code is correct
+    
+    Raises:
+        InputError
+        
+    '''
+    requests.post(url + "auth/passwordreset/request/v1",
+                        json = {'email': '1981686549@qq.com'})
+    bd = requests.get(url + 'backdoor/v1',
+                 params = SECRET).json()
+    re_code = bd['rcode'][-1]
+    res = requests.post(url + 'auth/passwordreset/reset/v1',
+                        json = {'reset_code': re_code,
+                                'new_password': "123456"})
+    assert res.status_code == 200
+    
+def test_auth_passwordreset_reset_invalid_new_password():
+    '''
+    
+    This test is to test when reset code is correct
+    
+    Raises:
+        InputError
+        
+    '''
+    requests.post(url + "auth/passwordreset/request/v1",
+                        json = {'email': '1981686549@qq.com'})
+    bd = requests.get(url + 'backdoor/v1',
+                 params = SECRET).json()
+    re_code = bd['rcode'][-1]
+    res = requests.post(url + 'auth/passwordreset/reset/v1',
+                        json = {'reset_code': re_code,
+                                'new_password': "12"})
+    assert res.status_code == InputError.code
 
 def test_auth_passwordreset_reset_wrong_reset_code():
     '''
